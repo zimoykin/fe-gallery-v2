34b6ca7d1d87514dd8e8e5d0234c60c03f4a0c3d
Dima <zimoykin@gmail.com>
2024-09-26 22:50:33 +0200

    added spinner, ui login screen

--- src/pages/profile.page.tsx
+++ src/pages/profile.page.tsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Avatar from '../components/avatar/avatar-component';
 import { IProfile } from '../interfaces/profile.interface';
 import ProfileInfoComponent from '../components/profile/profile-info.component';
+import CameraSpinner from '../components/camera-spinner/camera-spinner.component';
 const ProfilePage: React.FC = () => {
 
     const [profile, setProfile] = useState<IProfile | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     useEffect(() => {
+        setIsLoading(true);
         setTimeout(() => {
+            setIsLoading(false);
             setProfile({
                 id: '1',
                 name: 'John Doe',
@@ -24,16 +28,22 @@ const ProfilePage: React.FC = () => {
                 ]
             });
         }, 2000);
-    });
+    }, []);
 
     return (<div
         className='flex w-full shadow-lg h-full md:flex-row flex-col justify-start items-center md:p-1 md:gap-2'
     >
         {/* left side */}
+
         <div className='h-full md:w-1/3 w-full bg-main-bg p-2 shadow-2xl md:rounded-xl'>
             <Avatar url={profile?.url} />
             {profile && <ProfileInfoComponent profile={profile} />}
+
+            {isLoading && <div className='hidden md:flex md:w-1/3 bg-black bg-opacity-60 justify-center items-center md:h-full absolute top-0 left-0' >
+                <CameraSpinner size='medium' />
+            </div>}
         </div>
+
 
         {/* right side */}
         <div className='h-full md:w-2/3 w-full bg-secondary-bg-75 shadow-2xl md:rounded-xl'>
