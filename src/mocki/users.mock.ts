2786ab5d568892d2cd129b8702ec08ccc4deca33
Dima <zimoykin@gmail.com>
2024-10-16 20:20:11 +0200

    users mock fix

--- src/mocki/users.mock.ts
+++ src/mocki/users.mock.ts
@@ -15,6 +15,7 @@ export const MockUsers: IProfile[] = [
     url: "/avatar.jpg",
     privateAccess: false,
     userId: "1",
+    categories: ["sport", "food"],
   },
   {
     id: "2",
@@ -30,6 +31,7 @@ export const MockUsers: IProfile[] = [
     url: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraightStrand&accessoriesType=Blank&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue02&eyeType=Happy&eyebrowType=AngryNatural&mouthType=Default&skinColor=Brown",
     privateAccess: false,
     userId: "2",
+    categories: ["sport", "food"],
   },
   {
     id: "3",
@@ -45,6 +47,7 @@ export const MockUsers: IProfile[] = [
     url: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Sunglasses&hairColor=BrownDark&facialHairType=Blank&facialHairColor=Brown&clotheType=ShirtScoopNeck&clotheColor=Blue02&eyeType=Default&eyebrowType=Angry&mouthType=Sad&skinColor=Yellow",
     privateAccess: false,
     userId: "3",
+    categories: ["sport", "food"],
   },
   {
     id: "4",
@@ -60,6 +63,7 @@ export const MockUsers: IProfile[] = [
     url: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShaggyMullet&accessoriesType=Wayfarers&hairColor=Black&facialHairType=MoustacheFancy&facialHairColor=Red&clotheType=CollarSweater&clotheColor=PastelYellow&eyeType=Cry&eyebrowType=Angry&mouthType=Serious&skinColor=Tanned",
     privateAccess: false,
     userId: "4",
+    categories: ["sport", "food"],
   },
   {
     id: "5",
@@ -75,6 +79,7 @@ export const MockUsers: IProfile[] = [
     url: "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light",
     privateAccess: false,
     userId: "5",
+    categories: ["sport", "food"],
   },
   {
     id: "6",
@@ -90,5 +95,6 @@ export const MockUsers: IProfile[] = [
     url: "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairSides&accessoriesType=Blank&hairColor=SilverGray&facialHairType=Blank&facialHairColor=Blonde&clotheType=Hoodie&clotheColor=Heather&graphicType=Hola&eyeType=Squint&eyebrowType=UnibrowNatural&mouthType=Default&skinColor=Light",
     privateAccess: false,
     userId: "6",
+    categories: ["sport", "food"],
   },
 ];
